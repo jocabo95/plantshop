56ea2c3c3ffb5ea6a11ddc34e388d0acac123fe6
jocabo95 <camargojl1995@gmail.com>
2023-10-05 16:32:27 -0500

    persistance of user & logged status with local storage

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -5,19 +5,25 @@ export const AuthContext = createContext()
 
 const AuthContextComponent = ({children}) => {
 
-    const [loggedUser, setLoggedUser] = useState({})
-    const [loggedStatus, setLogedStatus] = useState(false)
+    const [loggedUser, setLoggedUser] = useState(JSON.parse(localStorage.getItem("loggedUser")) || {})
+    const [loggedStatus, setLogedStatus] = useState(JSON.parse(localStorage.getItem("loggedStatus") || false))
 
     // store user who loged in
     let userWhoLogged =(user)=>{
-        setLoggedUser(user)
-        setLogedStatus(true)        
+      setLoggedUser(user)
+      setLogedStatus(true) 
+    
+      localStorage.setItem("loggedUser", JSON.stringify(user))
+      localStorage.setItem("loggedStatus", JSON.stringify(true))
     }
     
     // clear logged user
     let logoutContext =()=>{
-        setLoggedUser({})
-        setLogedStatus(false)
+      setLoggedUser({})
+      setLogedStatus(false)
+
+      localStorage.removeItem("loggedUser")
+      localStorage.removeItem("loggedStatus")
     }
 
     const data = {
